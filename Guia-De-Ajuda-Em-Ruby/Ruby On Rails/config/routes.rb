2b71970c24fa4516db8affe662b9a3afb0a5a50d
Drack <61944386+Drack112@users.noreply.github.com>
2021-04-07 13:14:42 -0300

    Remove Rails

--- Guia-De-Ajuda-Em-Ruby/Ruby On Rails/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  #Nossa pagina principal
-  root "articles#index"
-  #Onde os arquivos HTML ou HTML.ERB vai estar
-  resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
